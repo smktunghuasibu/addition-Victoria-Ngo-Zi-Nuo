d6a28ee278ea50397f24ff70ef39d874fee04cff
Victoria-Ngo-Zi-Nuo <163367489+Victoria-Ngo-Zi-Nuo@users.noreply.github.com>
2024-08-23 09:21:10 +0800

    Update add.py1

--- add.py
+++ add.py
@@ -1,11 +1,11 @@
 def sum(x, y):
-    ................
-    ................
+    answer=x+y
+    return answer
 
 def addition():
     a = int(input('Enter 1st number: '))
-    b = int(........('Enter 2nd number: '))
-    total = sum(..........)
+    b = int(input('Enter 2nd number: '))
+    total = sum(a, b)
     print(f'Sum of {a} and {b} is {total}')
 
 ## JANGAN UBAH KOD DI BAWAH ##
